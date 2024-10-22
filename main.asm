30e2a9b4a288eb9d0affc195e4dee731f7f2b44d
zuzia <zuzannabrzeczek71@gmail.com>
2024-10-23 00:19:26 +0200

    koniec ale nie chce mi sie robic wszytkich ifow więcy dziala tylko dla a XD

--- main.asm
+++ main.asm
@@ -3,6 +3,7 @@
 .686
 .model flat
 extern _ExitProcess@4 : PROC
+extern _MessageBoxA@16 : PROC
 extern __write : PROC ; (dwa znaki podkreœlenia)
 extern __read : PROC ; (dwa znaki podkreœlenia)
 
@@ -11,6 +12,7 @@ public _main
 .data
 	tekst_pocz db 10, 'Proszê napisaæ jakiœ tekst '
 	db 'i nacisnac Enter', 10
+	tytulW 		dd  00790050h,0h
 
 	koniec_t db ?
 	magazyn db 80 dup (?)
@@ -21,39 +23,30 @@ public _main
 .code
 _main PROC
 
-	; wyœwietlenie tekstu informacyjnego
-	 mov ecx,(OFFSET koniec_t) - (OFFSET tekst_pocz) ; liczba znaków tekstu
-	 push ecx
-	 push OFFSET tekst_pocz ; adres tekstu
-	 push 1 ; nr urz¹dzenia (tu: ekran - nr 1)
-	 call __write ; wyœwietlenie tekstu pocz¹tkowego
-
-	 add esp, 12 ; usuniecie parametrów ze stosu
-
 	; czytanie wiersza z klawiatury
-
 	 push 80 ; maksymalna liczba znaków
 	 push OFFSET magazyn
 	 push 0 ; nr urz¹dzenia (tu: klawiatura - nr 0)
 	 call __read ; czytanie znaków z klawiatury
 	 add esp, 12 ; usuniecie parametrów ze stosu
 
-	; kody ASCII napisanego tekstu zosta³y wprowadzone
-	; do obszaru 'magazyn'
+
 	; funkcja read wpisuje do rejestru EAX liczbê
 	; wprowadzonych znaków
 	 mov liczba_znakow, eax
 
 	; rejestr ECX pe³ni rolê licznika obiegów pêtli
 	 mov ecx, eax
-	 mov bh, 0 ; indeks pocz¹tkowy
-	 mov bl, 0 ; indeks outputu
+	 mov ebx, 0 ; indeks pocz¹tkowy magazynu
+	 mov eax, 0 ; indeks outputu
+
+
 ptl: 
 	 mov dl, magazyn[ebx] ; pobranie kolejnego znaku
 	 cmp dl, '/'
 	 je dalej ; skok, gdy znak nie wymaga zamiany
 
-	 mov output[ebx], dl
+	 mov output[eax], dl
 	jmp koniec
 
 dalej:
@@ -68,35 +61,37 @@ dalej:
 	cmp dl, 'z'
 	je zmianaZ
 
-	mov magazyn[ebx], dl
-
 zmianaA:
-	mov output[ebx]	
+	mov output[eax], 185
+	inc eax
+	jmp koniec
 
 zmianaE:
+	mov output[eax], 'ê'
+	inc eax
+	jmp koniec
 
 zmianaZ:
+	mov output[eax], '¿'
+	inc eax
+	jmp koniec
 
 koniec:
-	
-	inc ebx ; inkrementacja indeksu
-
+	inc ebx
+	inc eax ; inkrementacja indeksu
 
 	loop ptl ; sterowanie pêtl¹
 
-	
-	 push 4
-	 push liczba_znakow
-	 push OFFSET magazyn
-	 push 0
-	 
-	 call _MessageBoxA@16 ; wyœwietlenie przekszta³conego tekstu
-	 
-	 add esp, 12 ; usuniecie parametrów ze stosu
+
+	push  4   ; utype
+	push OFFSET tytulW
+	push OFFSET output
+	push  0 ; hwnd
+	call _MessageBoxA@16
 
 
-	 push 0
-	 call _ExitProcess@4 ; zakoñczenie programu
+	push 0  ; exit code
+	call _ExitProcess@4
 
 _main ENDP
 END
